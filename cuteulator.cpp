bc79e53e4efc154c31df6936ac58dfc94b554f93
josefschodek <jschodek@outlook.com>
2024-08-09 15:01:39 -0500

    setup input for integer values

--- cuteulator.cpp
+++ cuteulator.cpp
@@ -1,5 +1,7 @@
 #include "cuteulator.h"
 #include "ui_cuteulator.h"
+#include <string>
+#include <vector>
 
 Cuteulator::Cuteulator(QWidget *parent)
     : QMainWindow(parent)
@@ -11,4 +13,80 @@ Cuteulator::Cuteulator(QWidget *parent)
 Cuteulator::~Cuteulator()
 {
     delete ui;
+}
+
+std::vector<std::string> expression;
+std::string term = "";
+
+// write expression to screen
+void Cuteulator::update()
+{
+    QString exp;
+    for(auto t : expression) {
+        exp += QString::fromStdString(t);
+    }
+    exp += QString::fromStdString(term);
+
+    ui->txtDisplay->setText(exp);
+}
+
+// button listeners
+void Cuteulator::on_btn0_clicked()
+{
+    term += "0";
+    update();
+}
+
+void Cuteulator::on_btn1_clicked()
+{
+    term += "1";
+    update();
+}
+
+void Cuteulator::on_btn2_clicked()
+{
+    term += "2";
+    update();
+}
+
+void Cuteulator::on_btn3_clicked()
+{
+    term += "3";
+    update();
+}
+
+void Cuteulator::on_btn4_clicked()
+{
+    term += "4";
+    update();
+}
+
+void Cuteulator::on_btn5_clicked()
+{
+    term += "5";
+    update();
+}
+
+void Cuteulator::on_btn6_clicked()
+{
+    term += "6";
+    update();
+}
+
+void Cuteulator::on_btn7_clicked()
+{
+    term += "7";
+    update();
+}
+
+void Cuteulator::on_btn8_clicked()
+{
+    term += "8";
+    update();
+}
+
+void Cuteulator::on_btn9_clicked()
+{
+    term += "9";
+    update();
 }
--- cuteulator.h
+++ cuteulator.h
@@ -17,7 +17,29 @@ public:
     Cuteulator(QWidget *parent = nullptr);
     ~Cuteulator();
 
+private slots:
+    void on_btn0_clicked();
+
+    void on_btn1_clicked();
+
+    void on_btn2_clicked();
+
+    void on_btn3_clicked();
+
+    void on_btn4_clicked();
+
+    void on_btn5_clicked();
+
+    void on_btn6_clicked();
+
+    void on_btn7_clicked();
+
+    void on_btn8_clicked();
+
+    void on_btn9_clicked();
+
 private:
     Ui::Cuteulator *ui;
+    void update();
 };
 #endif // CUTEULATOR_H
